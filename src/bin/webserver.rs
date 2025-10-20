a633e722f5aea16e9dde8d8f847421bf1053cd73
Michael Scherer <misc@redhat.com>
2025-10-20 17:20:12 +0200

    Add a JSON endpoint
    Since my goal is to automatically copy that to a shared google calendar,
    it is easier to have a json url that I do not need to parse than a ical.

--- src/bin/webserver.rs
+++ src/bin/webserver.rs
@@ -7,6 +7,7 @@ use axum::routing::get;
 use axum_response_cache::CacheLayer;
 use chrono::{Datelike, Local};
 use lunais::disruption_calendar::generate_ical;
+use lunais::disruption_calendar::generate_json;
 use lunais::index_page::IndexTemplate;
 use lunais::timezone_pair::TimezonePair;
 use std::env;
@@ -16,6 +17,7 @@ use tracing::Level;
 
 const PORT: u16 = 2507;
 const TEXT_CALENDAR: HeaderValue = HeaderValue::from_static("text/calendar");
+const APPLICATION_JSON: HeaderValue = HeaderValue::from_static("application/json");
 
 pub async fn index_handler() -> impl IntoResponse {
     let template = IndexTemplate::new();
@@ -43,6 +45,20 @@ pub async fn ical_handler(Path(tzp): Path<TimezonePair>) -> impl IntoResponse {
     (headers, i.to_string()).into_response()
 }
 
+pub async fn json_handler(Path(tzp): Path<TimezonePair>) -> impl IntoResponse {
+    let year = Local::now().naive_utc().date().year();
+
+    let mut d = Vec::new();
+    for y in year - 1..year + 3 {
+        d.append(&mut tzp.get_disruption_dates(y))
+    }
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::CONTENT_TYPE, APPLICATION_JSON);
+
+    (headers, generate_json(&d)).into_response()
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     tracing_subscriber::fmt()
@@ -62,6 +78,10 @@ async fn main() {
         .route(
             "/calendars/{*tzs}",
             get(ical_handler).layer(CacheLayer::with_lifespan(Duration::from_secs(24 * 60 * 60))),
+        )
+        .route(
+            "/json/{*tzs}",
+            get(json_handler).layer(CacheLayer::with_lifespan(Duration::from_secs(24 * 60 * 60))),
         )
         .layer(
             TraceLayer::new_for_http()
